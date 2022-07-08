a9fb07b31da27e666b35b2e780510b0496c48b1f
Steve Dowling <steve.dowling@fishtownanalytics.com>
2022-07-08 20:54:20 +0000

    added_more_clipboard

---
+++ clipboard/codegen_generate_base_model.sql
@@ -0,0 +1,4 @@
+{{ codegen.generate_base_model(
+    source_name='salesforce_data',
+    table_name='accounts'
+) }}
---
+++ clipboard/codegen_generate_source.sql
@@ -0,0 +1,6 @@
+{{ codegen.generate_source(
+    name='salesforce_data',
+    database_name='STEVE_D_RAW_DATA',
+    schema_name='SAMPLE_SALESFORCE_DATA',
+    generate_columns = True
+) }}
